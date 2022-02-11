e3da188e80638b08f825016845a03c81e55d66d7
Alessandro Pranzo <alessandropranzo@Alessandros-MacBook-Pro.local>
2022-02-11 21:45:07 +0100

    Added a check on the NIC, if it doesn't exists it gets created. Added IPV6INIT and MC_CONTROLLED no in the ifcfg-ethx configuration.

--- input.sh
+++ input.sh
@@ -7,15 +7,22 @@ INTERFACE=$1
 IP=$2
 SUB=$3
 
-if [[ ! -f "${PATH_IFCFG}"/"${1}"]]; then
-        echo "
+if [[ ! -f "${PATH_IFCFG}"/ifcfg-"${1}" ]]; then
+
+        echo "BOOTPROTO" >> \"${PATH_IFCFG}"/ifcfg-"${INTERFACE}"
+
+        echo "sed '/^BOOTPROTO/d' \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE} > \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE}.new
+        {
                 echo 'NAME=${INTERFACE}'
                 echo 'IPADDR=${IP}'
                 echo 'NETMASK=${SUB}'
                 echo 'BOOTPROTO=static'
-                echo 'DEVICE=${INTERFACE}'
-        > \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE}" >> ${PRE_FAILOVER_SCRIPT_LOCATION}
-        echo "ifcfg-${INTERFACE} did not exist and has been created." 
+                echo 'IPV6INIT=no'
+                echo 'NM_CONTROLLED=no'
+        } >> \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE}.new
+        cp \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE} \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE}.ori
+        mv \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE}.new \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE}
+        rm \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE}.ori" >> "${PRE_FAILOVER_SCRIPT_LOCATION}"
 else
         echo "sed '/^BOOTPROTO/d' \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE} > \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE}.new
         {
@@ -23,7 +30,10 @@ else
                 echo 'IPADDR=${IP}'
                 echo 'NETMASK=${SUB}'
                 echo 'BOOTPROTO=static'
+                echo 'IPV6INIT=no'
+                echo 'NM_CONTROLLED=no'
         } >> \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE}.new
         cp \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE} \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE}.ori
         mv \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE}.new \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE}
         rm \$PATH_TO_JOB_ID\$PATH_TO_IFCFG_FILES/ifcfg-${INTERFACE}.ori" >> "${PRE_FAILOVER_SCRIPT_LOCATION}"
+fi
